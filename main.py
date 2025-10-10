687e4e0059f3da7b4ed2de0e29a0ee274a3db5cf
Ivan-965 <skosiv13@gmail.com>
2025-10-10 21:04:10 +0300

    Command start and keyboard

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+
+from handlers.start import start
+
+load_dotenv()
+BOT_TOKEN = os.getenv("TOKEN")
+
+
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
